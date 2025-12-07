e6d22540af0a585456743af09113be616a681fcf
Vitor Levi <levivitor8@gmail.com>
2025-12-07 11:34:01 -0300

    style: change colors gradient

--- portfolio/src/components/features/about/metrics/Metrics.tsx
+++ portfolio/src/components/features/about/metrics/Metrics.tsx
@@ -5,21 +5,21 @@ const Metrics = () => {
         <Metricas>
             <Data>
                 <GradientText
-                  colors={["#581dc5", "#4079ff", "#581dc5", "#4079ff", "#581dc5"]}
-                  animationSpeed={8}
-                  showBorder={false}
-                  className="metrics"
+                    colors={["#15082e", "#3b155c", "#210e43", "#1c024f", "#130a24"]}
+                    animationSpeed={8}
+                    showBorder={false}
+                    className="metrics"
                 >
-                    3+
+                    2+
                 </GradientText>
                 <p>Anos de Experiência</p>
             </Data>
             <Data>
                 <GradientText
-                  colors={["#581dc5", "#4079ff", "#581dc5", "#4079ff", "#581dc5"]}
-                  animationSpeed={8}
-                  showBorder={false}
-                  className="metrics"
+                    colors={["#15082e", "#3b155c", "#210e43", "#1c024f", "#130a24"]}
+                    animationSpeed={8}
+                    showBorder={false}
+                    className="metrics"
                 >
                     30+
                 </GradientText>
@@ -27,12 +27,12 @@ const Metrics = () => {
             </Data>
             <Data>
                 <GradientText
-                  colors={["#581dc5", "#4079ff", "#581dc5", "#4079ff", "#581dc5"]}
-                  animationSpeed={8}
-                  showBorder={false}
-                  className="metrics"
+                    colors={["#15082e", "#3b155c", "#210e43", "#1c024f", "#130a24"]}
+                    animationSpeed={8}
+                    showBorder={false}
+                    className="metrics"
                 >
-                        1200+
+                    1300+
                 </GradientText>
                 <p>Commits no GitHub</p>
             </Data>
